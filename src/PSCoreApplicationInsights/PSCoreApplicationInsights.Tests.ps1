da4e1985c23de5dded4bddf1a396a33f44358ccf
Léon <leonboers@gmail.com>
2022-07-06 20:35:00 +0200

    Updated pester test

--- src/PSCoreApplicationInsights/PSCoreApplicationInsights.Tests.ps1
+++ src/PSCoreApplicationInsights/PSCoreApplicationInsights.Tests.ps1
@@ -3,11 +3,47 @@ Import-Module .\src\PSCoreApplicationInsights\PSCoreApplicationInsights.psm1 -Fo
 Describe "Creating a Client" {
     New-ApplicationInsightsClient -InstrumentationKey "c323cf10-da34-4a73-9eac-47dad64d840b"
 
-    It "Should create a client"{
+    It "should exist" {
+        $global:AIClient | should -not -BeNullOrEmpty
+    }
+
+    It "Should create a TelemetryClient" {
         $global:AIClient.GetType().Name | should -BeExactly "TelemetryClient"
     }
 
     It "Should have the set instrumentationkey" {
         $global:AIClient.InstrumentationKey | Should -Be "c323cf10-da34-4a73-9eac-47dad64d840b"
     }
+}
+
+Describe "Client Properties" {
+
+    It "Should have a default user Agent" {
+        $global:AIClient.Context.User.UserAgent | should -Not -BeNullOrEmpty
+    }
+
+    It "Should have a specific default user Agent" {
+        $global:AIClient.Context.User.UserAgent | should -Be ("PS $($psversiontable.PSEdition) $($psversiontable.PSVersion)")
+    }
+
+    It "Should have a specific default user" {
+        $global:AIClient.Context.User.AuthenticatedUserId | should -Be (whoami)
+    }
+
+
+    $updatedUserInformation = @{
+        AuthenticatedUserId = "MyNewUserId12345"
+        UserAgent           = "Modified User Agent"
+    }
+
+    # Set-ApplicationInsightsClientInformation -UserInformation $updatedUserInformation
+
+    # It "Should update the Authenticated User Id"{
+    #     $global:AIClient.Context.User.AuthenticatedUserId | should -Be "MyNewUserId12345"
+    # }
+
+    # It "Should update the UserAgent" {
+    #     $global:AIClient.Context.User.UserAgent | should -Be "Modified User Agent"
+    # }
+
 }
